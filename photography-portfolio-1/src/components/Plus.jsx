bd048af6db602613c14b87b7ebe052166e7dfcbc
Portfoliify <154722801+Portfoliify@users.noreply.github.com>
2024-01-19 22:12:55 +0530

    Delete photography-portfolio-1 directory

--- photography-portfolio-1/src/components/Plus.jsx
+++
@@ -1,7 +0,0 @@
-// import React from "react";
-
-const Plus = () => {
-  return <p id="plus">+</p>;
-};
-
-export default Plus;
--- photography-portfolio-1/src/components/WorkAlbums.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const WorkAlbums = () => {
-  return (
-    <div>WorkAlbums</div>
-  )
-}
-
-export default WorkAlbums
